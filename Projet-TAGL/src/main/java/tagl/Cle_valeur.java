db788870d0a108191b35c583673b8a6e29daac1e
clouet <myriam.clouet@e.ujf-grenoble.fr>
2016-04-01 15:46:41 +0200

    Mise en place des fonctionalités simples (set, get, del, setnx) pour des couples de clé-valeur de type strings.

--- Projet-TAGL/src/main/java/tagl/Cle_valeur.java
+++ Projet-TAGL/src/main/java/tagl/Cle_valeur.java
@@ -3,4 +3,48 @@ package tagl;
 public class Cle_valeur {
 	private String cle;
 	private String valeur;
+	
+	
+	/**
+	 * Constructeur d'un couple clé-valeur
+	 * @param c le nom de la clé
+	 * @param v la valeur à associer à la clé
+	 */
+	public Cle_valeur(String c, String v){
+		cle = c;
+		valeur = v;
+	}
+
+	/**
+	 * Fonction permettant de récupérer le nom de la clé
+	 * @return le nom de la clé
+	 */
+	public String getCle() {
+		return cle;
+	}
+
+	/**
+	 * Fonction permettant de modifier le nom de la clé
+	 * @param cle le nouveau nom de la clé
+	 */
+	public void setCle(String cle) {
+		this.cle = cle;
+	}
+
+	/**
+	 * Fonction permettant de récupérer la valeur qui est associé à la clé
+	 * @return
+	 */
+	public String getValeur() {
+		return valeur;
+	}
+
+	/**
+	 * Fonction permettant de modifier la valeur qui est associée à la clé
+	 * @param valeur la nouvelle valeur
+	 */
+	public void setValeur(String valeur) {
+		this.valeur = valeur;
+	}
+	
 }
--- Projet-TAGL/src/main/java/tagl/Gestion_cle_valeur.java
+++ Projet-TAGL/src/main/java/tagl/Gestion_cle_valeur.java
@@ -14,9 +14,48 @@ public class Gestion_cle_valeur {
 	 * @return 1 si l'enregistrement à pu être fait, 0 sinon
 	 */
 	public int set(String cle, String valeur){
-		
-		return 0;
+		int reussi = 0;
+		// si la clé à déjà été enregistrée avant
+		if(cleExists(cle)){
+			// on récupère sa position dans la liste
+			int pos = posCle(cle);
+			Cle_valeur tmp = list.get(pos);
+			// on modifie la valeur associée à cette clé dans la liste
+			tmp.setValeur(valeur);
+			// on met à jour la liste
+			list.set(pos, tmp);
+			reussi = 1;
+		}
+		// sinon
+		else{
+			// on crée un nouveau couple (clé, valeur) et on l'ajoute à la liste
+			Cle_valeur tmp = new Cle_valeur(cle, valeur);
+			list.add(tmp);
+			reussi = 1;
+		}
+		return reussi;
 	}
+	
+	
+	/**
+	 * Fonction permettant d'enregistrer une clé associée à une valeur. 
+	 * La clé ne doit pas avoir déja été enregistrée.
+	 * @param cle le nom de la clé
+	 * @param valeur la valeur à associer à la clé
+	 * @return 1 si l'enregistrement à pu être fait, 0 sinon
+	 */
+	public int setnx(String cle, String valeur){
+		int reussi = 0;
+		// si la clé à déjà été enregistrée avant
+		if(!cleExists(cle)){
+			// on crée un nouveau couple (clé, valeur) et on l'ajoute à la liste
+			Cle_valeur tmp = new Cle_valeur(cle, valeur);
+			list.add(tmp);
+			reussi = 1;
+		}
+		return reussi;
+	}
+	
 	
 	/**
 	 * Fonction permettant de récupérer la valeur qui est associé à une clé
@@ -24,6 +63,66 @@ public class Gestion_cle_valeur {
 	 * @return la valeur associée à la clé
 	 */
 	public String get(String cle){
-		return null;
+		String valeur = null;
+		if(cleExists(cle)){
+			int pos = posCle(cle);
+			Cle_valeur tmp = list.get(pos);
+			valeur = tmp.getValeur();
+		}
+		return valeur;
 	}
+	
+	
+	/**
+	 * Fonction permettant d'enlever un couple (clé,valeur) de la liste
+	 * @param cle la clé dont on veut enlever l'enregistrement
+	 * @return 1 si le couple à bien été enlevé, 0 sinon
+	 */
+	public int del(String cle){
+		int reussi = 0;
+		if(cleExists(cle)){
+			int pos = posCle(cle);
+			list.remove(pos);
+			reussi = 1;
+		}
+		return reussi;
+	}
+	
+	/**
+	 * Fonction permettant de vérifier si une clé à déja été enregistrée
+	 * @param c la clé que l'on veut vérifier l'enregistrement
+	 * @return true si la clé à déja été enregistrée, false sinon
+	 */
+	private Boolean cleExists(String c){
+		Boolean result = false;
+		if(list != null){
+			for(int i = 0; i < list.size(); i++){
+				Cle_valeur couple = list.get(i);
+				if(couple.getCle().equals(c)) result = true;
+			}
+		}
+		return result;
+	}
+	
+	/**
+	 * Fonction permettant de récupérer la position d'une clé dans la liste d'enregistrement
+	 * @param c la clé dont on veut connaitre la position
+	 * @return la position de la clé dans la liste d'enregistrement ou -1 si la clé n'existe pas dans la liste d'enregistrement.
+	 */
+	private int posCle(String c){
+		int pos = -1;
+		Boolean find = false;
+		if(list != null){
+			int i = 0;
+			while (i < list.size() && !find){
+				Cle_valeur couple = list.get(i);
+				if(couple.getCle().equals(c)) find = true;
+				else i++;
+			}
+			pos = i;
+		}
+		return pos;
+	}
+	
+	
 }
