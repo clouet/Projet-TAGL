505621d62e1eb92f28fd84de36234a6feeeb609e
clouet <myriam.clouet@e.ujf-grenoble.fr>
2016-04-01 11:58:04 +0200

    javadoc gestion_clé_valeur

--- Projet-TAGL/src/main/java/tagl/Gestion_cle_valeur.java
+++ Projet-TAGL/src/main/java/tagl/Gestion_cle_valeur.java
@@ -5,11 +5,24 @@ import java.util.ArrayList;
 public class Gestion_cle_valeur {
 	private ArrayList<Cle_valeur> list;
 	
+	
+	/**
+	 * Fonction permettant d'enregistrer une clé associée à une valeur. 
+	 * Si la clé existait avant, la valeur est mise à jour
+	 * @param cle le nom de la clé
+	 * @param valeur la valeur à associer à la clé
+	 * @return 1 si l'enregistrement à pu être fait, 0 sinon
+	 */
 	public int set(String cle, String valeur){
 		
 		return 0;
 	}
 	
+	/**
+	 * Fonction permettant de récupérer la valeur qui est associé à une clé
+	 * @param cle le nom de la clé dont on veut récupérer la valeur qui lui est associée
+	 * @return la valeur associée à la clé
+	 */
 	public String get(String cle){
 		return null;
 	}
