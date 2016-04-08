569e3756ad6d97c7b8d770c145119552ec978fa0
clouet <myriam.clouet@e.ujf-grenoble.fr>
2016-04-08 09:49:43 +0200

    Mise en place de nouvelles fonctionnalités et correction de bug (set avec null pointeur exception)

--- Projet-TAGL/src/main/java/tagl/Gestion_cle_valeur.java
+++ Projet-TAGL/src/main/java/tagl/Gestion_cle_valeur.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 public class Gestion_cle_valeur {
 	private ArrayList<Cle_valeur> list;
 	
+	public Gestion_cle_valeur(){
+		if(list == null) list = new ArrayList<>();
+	}
 	
 	/**
 	 * Fonction permettant d'enregistrer une clé associée à une valeur. 
@@ -87,6 +90,144 @@ public class Gestion_cle_valeur {
 		}
 		return reussi;
 	}
+	
+	/**
+	 * Fonction permettant d'incrémenter la valeur associée à une clé si cette valeur est un entier.
+	 * Si la clé n'exsiste pas, un nouveau couple (clé,valeur) est crée, avec comme valeur 1.
+	 * @param cle la clé dont on veut d'incrémenter la valeur
+	 * @return la nouvelle valeur associée à la clé, ou lève une erreur si la valeur associée à la clé n'est pas un entier
+	 */
+	public int incr(String cle) throws NumberFormatException{
+		int reussi = 0;
+		if(!cleExists(cle)){
+			Cle_valeur cv = new Cle_valeur(cle, "1");
+			list.add(cv);
+			reussi = 1;
+		}
+		else{
+			int pos = posCle(cle);
+			Cle_valeur cv = list.get(pos);
+			String valeur = cv.getValeur();
+			try{
+				int n = Integer.parseInt(valeur);
+				n = n + 1;
+				cv.setValeur(String.valueOf(n));
+				reussi = n;
+			}catch(NumberFormatException n){
+				throw n;
+			}
+		}
+		return reussi;
+	}
+	
+	/**
+	 * Fonction permettant d'augmenter la valeur associée à une clé d'un certain entier, si cette valeur est un entier.
+	 * Si la clé n'exsiste pas, un nouveau couple (clé,valeur) est crée, avec comme valeur l'entier donné.
+	 * @param cle la clé dont on veut augmenter la valeur
+	 * @param i l'entier dont on veut augmenter la valeur associée à la clé
+	 * @return la nouvelle valeur associée à la clé, ou lève une erreur si la valeur associée à la clé n'est pas un entier
+	 */
+	public int incrBy(String cle, int i) throws NumberFormatException{
+		int reussi = 0;
+		if(!cleExists(cle)){
+			Cle_valeur cv = new Cle_valeur(cle, String.valueOf(i));
+			list.add(cv);
+			reussi = i;
+		}
+		else{
+			int pos = posCle(cle);
+			Cle_valeur cv = list.get(pos);
+			String valeur = cv.getValeur();
+			try{
+				int n = Integer.parseInt(valeur);
+				n = n + i;
+				cv.setValeur(String.valueOf(n));
+				reussi = n;
+			}catch(NumberFormatException n){
+				throw n;
+			}
+		}
+		return reussi;
+	}
+	
+	
+	/**
+	 * Fonction permettant de décrémenter la valeur associée à une clé si cette valeur est un entier.
+	 * Si la clé n'exsiste pas, un nouveau couple (clé,valeur) est crée, avec comme valeur -1.
+	 * @param cle la clé dont on veut décrémenter la valeur
+	 * @return la nouvelle valeur associée à la clé, ou lève une erreur si la valeur associée à la clé n'est pas un entier
+	 */
+	public int decr(String cle) throws NumberFormatException{
+		int reussi = 0;
+		if(!cleExists(cle)){
+			Cle_valeur cv = new Cle_valeur(cle, "-1");
+			list.add(cv);
+			reussi = -1;
+		}
+		else{
+			int pos = posCle(cle);
+			Cle_valeur cv = list.get(pos);
+			String valeur = cv.getValeur();
+			try{
+				int n = Integer.parseInt(valeur);
+				n = n - 1;
+				cv.setValeur(String.valueOf(n));
+				reussi = n;
+			}catch(NumberFormatException n){
+				throw n;
+			}
+		}
+		return reussi;
+	}
+	
+	/**
+	 * Fonction permettant de diminuer la valeur associée à une clé  si cette valeur est un entier.
+	 * Si la clé n'exsiste pas, un nouveau couple (clé,valeur) est crée, avec comme valeur -1.
+	 * @param cle la clé dont on veut décrémenter la valeur
+	 * @param i l'entier dont on veut diminuer la valeur associée à la clé
+	 * @return la nouvelle valeur associée à la clé, ou lève une erreur si la valeur associée à la clé n'est pas un entier
+	 */
+	public int decrBy(String cle, int i) throws NumberFormatException{
+		int reussi = 0;
+		if(!cleExists(cle)){
+			String val = String.valueOf(-i);
+			Cle_valeur cv = new Cle_valeur(cle, val);
+			list.add(cv);
+			reussi = -i;
+		}
+		else{
+			int pos = posCle(cle);
+			Cle_valeur cv = list.get(pos);
+			String valeur = cv.getValeur();
+			try{
+				int n = Integer.parseInt(valeur);
+				n = n - i;
+				cv.setValeur(String.valueOf(n));
+				reussi = n;
+			}catch(NumberFormatException n){
+				throw n;
+			}
+		}
+		return reussi;
+	}
+	
+	/**
+	 * Fonction permettant de vérifier si une clé est enregistrée.
+	 * @param cle la clé que l'on veut vérifier
+	 * @return 1 si la clé est enregistrée, 0 sinon
+	 */
+	public int exists(String cle){
+		int exist = 0;
+		if(cleExists(cle)) exist = 1;
+		return exist;
+	}
+	
+	
+	/*public int rename(String cle, String newname){
+		int reussi = 0;
+		
+		return reussi;
+	}*/
 	
 	/**
 	 * Fonction permettant de vérifier si une clé à déja été enregistrée
